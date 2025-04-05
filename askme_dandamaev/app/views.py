030e427518e9dd11fe26fd7b32d4179039ea737d
saiddandamaevv <saiddandamaev6@gmail.com>
2025-04-06 01:32:09 +0300

    ДЗ2

--- askme_dandamaev/app/views.py
+++ askme_dandamaev/app/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.paginator import Paginator
 import copy
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 QUESTIONS = [
     {
@@ -19,24 +20,35 @@ ANSWERS = [
     } for i in range(10)
 ] 
 
+def paginate(objects_list, request, per_page=10):
+    try:
+        page_num = int(request.GET.get('page', 1))
+    except(TypeError, ValueError):
+        page_num = 1
+    paginator = Paginator(objects_list, per_page)
+    try:
+        page = paginator.page(page_num)
+    except PageNotAnInteger:
+        page = paginator.page(1)
+    except EmptyPage:
+        if page_num < 0:
+            page = paginator.page(1)
+        else:
+            page = paginator.page(paginator.num_pages)
+    return page
+
 # Create your views here.
 def index(request):
-    page_num = int(request.GET.get('page', 1))
-    paginator = Paginator(QUESTIONS, per_page = 5)
-    page = paginator.page(page_num)
+    page = paginate(QUESTIONS, request, 5)
     return render(request, template_name='index.html', context={'questions' : page.object_list, 'page_obj' : page})
 
 def hot(request):
     q = copy.deepcopy(QUESTIONS)[::-1]
-    page_num = int(request.GET.get('page', 1))
-    paginator = Paginator(q, per_page = 5)
-    page = paginator.page(page_num)
+    page = paginate(q, request, 5)
     return render(request, template_name='hot.html', context={'questions' : page.object_list, 'page_obj' : page})
 
 def question(request, question_id):
-    page_num = int(request.GET.get('page', 1))
-    paginator = Paginator(ANSWERS, per_page = 5)
-    page = paginator.page(page_num)
+    page = paginate(ANSWERS, request, 5)
     return render(request, template_name='single_question.html', context={'question' : QUESTIONS[question_id], 'answers' : page.object_list, 'page_obj': page})
 
 def login(request):
@@ -47,3 +59,10 @@ def signup(request):
 
 def ask(request):
     return render(request, template_name='ask.html')
+
+def tag(request, tag):
+    page = paginate(QUESTIONS, request, 5)
+    return render(request, template_name="questions_by_tag.html", context={'questions' : page.object_list, 'page_obj' : page, 'tag' : tag})
+
+def profile(request):
+    return render(request, template_name='profile.html')
--- askme_dandamaev/askme_dandamaev/urls.py
+++ askme_dandamaev/askme_dandamaev/urls.py
@@ -21,9 +21,11 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('/hot', views.hot, name='hot'),
-    path('/question/<int:question_id>', views.question, name='question'),
-    path('/login', views.login, name='login'),
-    path('/sighup', views.signup, name='signup'),
-    path('/ask', views.ask, name='ask')
+    path('hot/', views.hot, name='hot'),
+    path('question/<int:question_id>/', views.question, name='question'),
+    path('login/', views.login, name='login'),
+    path('sighup/', views.signup, name='signup'),
+    path('ask/', views.ask, name='ask'),
+    path('questions_by_tag/<str:tag>/', views.tag, name='tag'),
+    path('profile/', views.profile, name = 'profile')
 ]
